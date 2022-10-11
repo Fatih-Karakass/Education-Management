4a359c6fe989d00a5a60ff9f313c3e9650d8a5c5
Ergun Deniz Buyruk <ergun.buyruk@arfitect.com>
2022-10-11 21:41:32 +0300

    initial project and set sbContext

---
+++ Ders1/DataAccess/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ders1.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    }
+}
