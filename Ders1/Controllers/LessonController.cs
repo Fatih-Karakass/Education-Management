f5e9bb4c391c942e03469dadf2fd58efa2d6fb5f
Fatih Karakaş <kfatih2744@gmail.com>
2022-10-17 20:34:06 +0300

    Lesson Create And Lesson Remove Finished

--- Ders1/Controllers/LessonController.cs
+++ Ders1/Controllers/LessonController.cs
@@ -17,5 +17,57 @@ namespace Ders1.Controllers
             IEnumerable<Lesson> list = _db.Lesson.ToList();
             return View(list);
         }
+        public IActionResult Create()
+        {
+            Lesson lesson = new Lesson();
+            return View(lesson);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(Lesson lesson)
+        {
+            if (!ModelState.IsValid)
+            {
+            return View(lesson);
+
+            }
+            _db.Lesson.Add(lesson);
+            _db.SaveChanges();
+            
+            return RedirectToAction(nameof(Index));
+
+        }
+        public IActionResult Remove(int? id)
+        {
+            if(id==null|| id == 0)
+            {
+                return NotFound();
+            }
+            Lesson lesson = _db.Find<Lesson>(id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+            return View(lesson);
+        }
+        [HttpPost, ActionName("Remove")]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult RemovePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            
+            Lesson lesson = _db.Find<Lesson>(id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+            _db.Lesson.Remove(lesson);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
     }
 }
