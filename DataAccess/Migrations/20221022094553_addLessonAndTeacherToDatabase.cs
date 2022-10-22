3106e922647f288936efbab869fa46602db45f52
Ergun-Deniz-Buyruk <ergunndenizbuyruk@gmail.com>
2022-10-22 13:11:21 +0300

    convert to 5-tier artitechture

---
+++ DataAccess/Migrations/20221022094553_addLessonAndTeacherToDatabase.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class addLessonAndTeacherToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lesson",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LessonName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LessonTeacher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LessonCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lesson", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lesson");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
---
+++ Utility/Utility.cs
@@ -0,0 +1,10 @@
+﻿namespace Utility
+{
+    public class Utility
+    {
+        public const string ROLE_ADMIN = "ROLE_ADMIN";
+        public const string ROLE_USER = "ROLE_USER";
+
+
+    }
+}
